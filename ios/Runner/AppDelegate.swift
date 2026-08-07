df8551035c9a01c257f0a369f8c6ce3c7e34fad9
David Jonah <dyildum@gmail.com>
2026-08-07 14:17:34 +0100

    fix: remove GoogleMaps import from AppDelegate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import Flutter
 import UIKit
-import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
@@ -8,7 +7,6 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNB4")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
   func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
