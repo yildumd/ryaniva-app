15bcf7d05049f3b2faabdd27e1c0c790c8f4070d
David Jonah <dyildum@gmail.com>
2026-08-10 11:30:20 +0100

    fix: use standard Flutter AppDelegate without SceneDelegate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,14 +3,11 @@ import UIKit
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
-    lazy var flutterEngine = FlutterEngine(name: "ryaniva_engine")
-    
-    override func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-    ) -> Bool {
-        flutterEngine.run()
-        GeneratedPluginRegistrant.register(with: self.flutterEngine)
-        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-    }
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
 }
